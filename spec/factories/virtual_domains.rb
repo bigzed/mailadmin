976aa63580d6d4350686cc4dd294f39fa26ed680
Steve Dierker <kiwi@obstkiste.org>
2013-10-26 15:00:00 +0200

    Added specs for virtial domains

--- spec/factories/virtual_domains.rb
+++ spec/factories/virtual_domains.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :example_com, class: VirtualDomain do
     name "example.com"
   end
+
+  factory :malformed_com, class: VirtualDomain do
+    name "malformedcom"
+  end
 end
--- spec/models/virtual_domain_spec.rb
+++ spec/models/virtual_domain_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
 describe VirtualDomain do
-  pending "add some examples to (or delete) #{__FILE__}"
+  after(:each) do
+    # Clean up
+    VirtualDomain.delete_all
+  end
+
+  describe 'name' do
+    context 'a valid domain' do
+      before(:each) do
+        @domain = FactoryGirl.build(:example_com)
+      end
+
+      it 'be excepted' do
+        @domain.save.should be
+      end
+    end
+
+    context 'a duplicate domain' do
+      before(:each) do
+        FactoryGirl.create(:example_com)
+        @duplicate = FactoryGirl.build(:example_com)
+      end
+
+      it 'be rejected' do
+        @duplicate.save.should not_be
+      end
+    end
+
+    context 'a malformed domain' do
+      before(:each) do
+        @malformed = FactoryGirl.build(:malformed_com)
+      end
+      it 'be rejected' do
+        @malformed.save.should not_be
+      end
+    end
+  end
 end
