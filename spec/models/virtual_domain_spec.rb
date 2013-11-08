a5f32cd97fe6f07303c06d9c2e1b3f635ceaefd6
Steve Dierker <kiwi@obstkiste.org>
2013-11-08 14:52:46 +0100

    Shortened specs for virtual_domain

--- spec/models/virtual_domain_spec.rb
+++ spec/models/virtual_domain_spec.rb
@@ -1,41 +1,10 @@
 require 'spec_helper'
 
 describe VirtualDomain do
-  after(:each) do
-    # Clean up
-    VirtualDomain.delete_all
-  end
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 
   describe 'name' do
-    context 'an empty name' do
-      it 'be rejected' do
-        VirtualDomain.new.save.should_not be
-      end
-    end
-
-    context 'a valid domain' do
-      before(:each) do
-        @domain = FactoryGirl.build(:example_com)
-      end
-
-      it 'be excepted' do
-        @domain.save.should be
-      end
-    end
-
-    context 'a duplicate domain' do
-      before(:each) do
-        FactoryGirl.create(:example_com)
-        @duplicate = FactoryGirl.build(:example_com)
-      end
-
-      it 'be rejected' do
-        @duplicate.save.should_not be
-      end
-    end
-
-    context 'a malformed domain' do
-      it 'be rejected'
-    end
+    it 'validates format of domain'
   end
 end
