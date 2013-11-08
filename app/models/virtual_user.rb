6b801ec2f30ca33a6a09ffffb35578bf03497885
Steve Dierker <kiwi@obstkiste.org>
2013-11-08 14:52:28 +0100

    Added base model for virtual_user

--- app/models/virtual_user.rb
+++ app/models/virtual_user.rb
@@ -1,2 +1,4 @@
 class VirtualUser < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
--- spec/factories/virtual_users.rb
+++ spec/factories/virtual_users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
-  factory :johndoe, class: VirtualUser do
-    email "johndoe@example.com"
-    password "{SSHA}Ak6YMDROTBvM4ZZ0aLv87L3LpyKzxgba"
+  factory :john_doe, class: VirtualUser do
+    email 'johndoe@example.com'
+    password 'password'
   end
 end
